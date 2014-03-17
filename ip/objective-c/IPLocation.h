fb2183178c845a9b92a6538e97ef0a05b48c7cf1
ilsanbao <ilsanboo@gmail.com>
2014-03-17 21:44:40 +0800

    add objective-c code

---
+++ ip/objective-c/IPLocation.h
@@ -0,0 +1,13 @@
+//
+//  IPLocation.h
+//  objcdemos
+//
+//  Created by 范茹宽 on 14-3-14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IPLocation : NSObject
+
+@end
---
+++ ip/objective-c/IPLocation.m
@@ -0,0 +1,13 @@
+//
+//  IPLocation.m
+//  objcdemos
+//
+//  Created by 范茹宽 on 14-3-14.
+//
+//
+
+#import "IPLocation.h"
+
+@implementation IPLocation
+
+@end
