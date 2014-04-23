9c8da1dded4b708d7643631159aad267709cc259
ilsanbao <ilsanboo@gmail.com>
2014-04-23 21:52:37 +0800

    update luajit code

--- ip/luajit/ip.lua
+++ ip/luajit/ip.lua
@@ -14,56 +14,38 @@ function byteToUint32(a,b,c,d)
 	_int = _int + bit.lshift(b, 16)
 	_int = _int + bit.lshift(c, 8)
 	_int = _int + d
-	return _int
+	if _int >= 0 then
+        return _int
+    else
+        return _int + math.pow(2, 32)
+    end
 end
 
-function Split(szFullString, szSeparator)
-	local nFindStartIndex = 1
-	local nSplitIndex = 1
-	local nSplitArray = {}
-	while true do
-	   local nFindLastIndex = string.find(szFullString, szSeparator, nFindStartIndex)
-	   if not nFindLastIndex then
-	    nSplitArray[nSplitIndex] = string.sub(szFullString, nFindStartIndex, string.len(szFullString))
-	    break
-	   end
-	   nSplitArray[nSplitIndex] = string.sub(szFullString, nFindStartIndex, nFindLastIndex - 1)
-	   nFindStartIndex = nFindLastIndex + string.len(szSeparator)
-	   nSplitIndex = nSplitIndex + 1
-	end
-	return nSplitArray
-end
-
-local ipBinaryFilePath = "/home/software/17monipdb.dat"
+local ipBinaryFilePath = "../../../../17monipdb.dat"
 
 function IpLocation(ipstr)
 	local ip_uint32 = ip2long(ipstr)
-
 	local file = io.open(ipBinaryFilePath)
 	if file == nil then
 		return nil
 	end
 
 	local str = file:read(4)
-
 	local offset_len = byteToUint32(string.byte(str, 1), string.byte(str, 2),string.byte(str, 3),string.byte(str, 4))
 
 	local indexBuffer = file:read(offset_len - 4)
 
-	local tmp_offset = 118 * 4 -- 这里是写死的
-
+	local tmp_offset = string.match(ipstr, "(%d+)") * 4 
 	local start_len = byteToUint32(string.byte(indexBuffer, tmp_offset + 4), string.byte(indexBuffer, tmp_offset + 3), string.byte(indexBuffer, tmp_offset + 2), string.byte(indexBuffer, tmp_offset + 1))
 
 	local max_comp_len = offset_len - 1028
-
 	start = start_len * 8 + 1024 + 1
-
-	local index_offset = 0
-	local index_length = 0
+    local find_uint32 = 0
+	local index_offset = -1
+	local index_length = -1
 	while start < max_comp_len do
-		local find_uint32 = byteToUint32(string.byte(indexBuffer, start), string.byte(indexBuffer, start+1),string.byte(indexBuffer, start+2),string.byte(indexBuffer, start+3))
-
-		if ip_uint32 <= find_uint32  then
+        find_uint32 = byteToUint32(string.byte(indexBuffer, start), string.byte(indexBuffer, start+1),string.byte(indexBuffer, start+2),string.byte(indexBuffer, start+3))
+        if ip_uint32 <= find_uint32  then
 			index_offset = byteToUint32(0, string.byte(indexBuffer, start+6),string.byte(indexBuffer, start+5),string.byte(indexBuffer, start+4))
 			index_length = string.byte(indexBuffer, start+7)
 			break
@@ -71,7 +53,7 @@ function IpLocation(ipstr)
 		start = start + 8
 	end
 
-	if index_offset == 0 or index_length == 0 then
+	if index_offset == -1 or index_length == -1 then
 		return nil
 	end
 
@@ -82,4 +64,4 @@ function IpLocation(ipstr)
 	return file:read(index_length)
 end
 
-print(IpLocation("118.26.235.100"))
+print(IpLocation("128.0.0.0"))
