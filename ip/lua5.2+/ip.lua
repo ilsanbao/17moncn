f15552684d2856af437a62efbb9338fe31528986
ilsanbao <ilsanbao@163.com>
2014-05-21 18:19:42 +0800

    lua 语言支持，要求5.2+版本

---
+++ ip/lua5.2+/ip.lua
@@ -0,0 +1,62 @@
+#!/usr/bin/env lua
+
+function byteToUint32(a,b,c,d)
+        local _int = 0
+	if a then
+		_int = _int +  bit32.lshift(a, 24)
+	end
+	_int = _int + bit32.lshift(b, 16)
+	_int = _int + bit32.lshift(c, 8)
+	_int = _int + d
+	if _int >= 0 then
+        return _int
+    else
+        return _int + math.pow(2, 32)
+    end
+end
+
+local ipBinaryFilePath = "/home/codebase/17mon/17monipdb.dat"
+
+function IpLocation(ipstr)
+    local ip1,ip2,ip3,ip4 = string.match(ipstr, "(%d+).(%d+).(%d+).(%d+)")
+    local ip_uint32 = byteToUint32(ip1, ip2, ip3, ip4)
+	local file = io.open(ipBinaryFilePath)
+	if file == nil then
+		return nil
+	end
+
+	local str = file:read(4)
+	local offset_len = byteToUint32(string.byte(str, 1), string.byte(str, 2),string.byte(str, 3),string.byte(str, 4))
+
+	local indexBuffer = file:read(offset_len - 4)
+
+	local tmp_offset = ip1 * 4 
+	local start_len = byteToUint32(string.byte(indexBuffer, tmp_offset + 4), string.byte(indexBuffer, tmp_offset + 3), string.byte(indexBuffer, tmp_offset + 2), string.byte(indexBuffer, tmp_offset + 1))
+
+	local max_comp_len = offset_len - 1028
+	start = start_len * 8 + 1024 + 1
+    local find_uint32 = 0
+	local index_offset = -1
+	local index_length = -1
+	while start < max_comp_len do
+        find_uint32 = byteToUint32(string.byte(indexBuffer, start), string.byte(indexBuffer, start+1),string.byte(indexBuffer, start+2),string.byte(indexBuffer, start+3))
+        if ip_uint32 <= find_uint32  then
+			index_offset = byteToUint32(0, string.byte(indexBuffer, start+6),string.byte(indexBuffer, start+5),string.byte(indexBuffer, start+4))
+			index_length = string.byte(indexBuffer, start+7)
+			break
+		end
+		start = start + 8
+	end
+
+	if index_offset == -1 or index_length == -1 then
+		return nil
+	end
+
+	local offset = offset_len + index_offset - 1024
+
+	file:seek("set", offset)
+
+	return file:read(index_length)
+end
+
+print(IpLocation("128.0.0.0"))
