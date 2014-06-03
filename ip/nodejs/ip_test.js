e1a4ccf83b18605f06b0ba3d2817dbc3d460e6b5
周知日 <chichou@ujs.edu.cn>
2014-06-04 00:25:33 +0800

    重构 Node.js 语言支持，优化逻辑，加入单元测试

--- ip/nodejs/ip_test.js
+++
@@ -1,9 +0,0 @@
-var IP = require('./ip');
-
-/* 异步方法 支持域名 */
-IP.find('116.226.54.188', function(data){
-    console.log(data);
-});
-
-/* 同步方法，不支持域名,仅支持IPv4 */
-console.log(IP.findSync('180.73.134.23'));
