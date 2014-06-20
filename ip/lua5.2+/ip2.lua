0049ccd9c7440b9ccd9ab511872523b2ac9d3481
小宝 <ilsanbao@163.com>
2014-06-20 16:52:51 +0800

    Update ip2.lua

--- ip/lua5.2+/ip2.lua
+++ ip/lua5.2+/ip2.lua
@@ -25,8 +25,7 @@ function loadAreaList()
     local file = io.open(ipBinaryFilePath)
     local str = file:read(4)
     
-    offset_len = byteToUint32(string.byte(str, 1), string.byte(str, 2)
-    ,string.byte(str, 3),string.byte(str, 4))
+    offset_len = byteToUint32(string.byte(str, 1), string.byte(str, 2),string.byte(str, 3),string.byte(str, 4))
     indexBuffer = file:read(offset_len - 4)
     
     file:seek("set", 4)
